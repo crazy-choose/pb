a60b6441bcd9d35617dd0613d0ddf2acaa5c9687
leo <it.include@foxmail.com>
2022-03-20 18:29:44 +0800

    2

--- stream/stream_grpc.pb.go
+++ stream/stream_grpc.pb.go
@@ -3,8 +3,8 @@
 package stream
 
 import (
-	model "/out/model"
 	context "context"
+	model "github.com/crazy-choose/pb/model"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
